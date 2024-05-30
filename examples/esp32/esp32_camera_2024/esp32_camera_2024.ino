0263e9a06fa8363d176ff37befca067bf2564b75
Samuele Sacchetti <sacchettisamuele@gmail.com>
2024-05-30 10:44:42 +0200

    - first commit

---
+++ examples/esp32/esp32_camera_2024/esp32_camera_2024.ino
@@ -0,0 +1,683 @@
+/* Includes ---------------------------------------------------------------- */
+#include <Shalym-project-1_inferencing.h>
+#include "edge-impulse-sdk/dsp/image/image.hpp"
+
+#include "esp_camera.h"
+//#include "SD_MMC.h"
+#include "sd_read_write.h"
+
+// Uncomment for using SD Card
+/*#define SD_MMC_CMD 15 
+#define SD_MMC_CLK 14
+#define SD_MMC_D0  2*/
+#define PIN_BLUE 2
+#define PIN_GREEN 15
+#define PIN_RED 32
+#define CAMERA_MODEL_WROVER_KIT
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+
+
+#if defined(CAMERA_MODEL_WROVER_KIT)
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  21
+#define SIOD_GPIO_NUM  26
+#define SIOC_GPIO_NUM  27
+
+#define Y9_GPIO_NUM    35
+#define Y8_GPIO_NUM    34
+#define Y7_GPIO_NUM    39
+#define Y6_GPIO_NUM    36
+#define Y5_GPIO_NUM    19
+#define Y4_GPIO_NUM    18
+#define Y3_GPIO_NUM    5
+#define Y2_GPIO_NUM    4
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM  23
+#define PCLK_GPIO_NUM  22
+
+// Uncomment to use other camera configurations
+/*
+#elif defined(CAMERA_MODEL_ESP_EYE)
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  4
+#define SIOD_GPIO_NUM  18
+#define SIOC_GPIO_NUM  23
+
+#define Y9_GPIO_NUM    36
+#define Y8_GPIO_NUM    37
+#define Y7_GPIO_NUM    38
+#define Y6_GPIO_NUM    39
+#define Y5_GPIO_NUM    35
+#define Y4_GPIO_NUM    14
+#define Y3_GPIO_NUM    13
+#define Y2_GPIO_NUM    34
+#define VSYNC_GPIO_NUM 5
+#define HREF_GPIO_NUM  27
+#define PCLK_GPIO_NUM  25
+
+#define LED_GPIO_NUM 22
+
+#elif defined(CAMERA_MODEL_M5STACK_PSRAM)
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM 15
+#define XCLK_GPIO_NUM  27
+#define SIOD_GPIO_NUM  25
+#define SIOC_GPIO_NUM  23
+
+#define Y9_GPIO_NUM    19
+#define Y8_GPIO_NUM    36
+#define Y7_GPIO_NUM    18
+#define Y6_GPIO_NUM    39
+#define Y5_GPIO_NUM    5
+#define Y4_GPIO_NUM    34
+#define Y3_GPIO_NUM    35
+#define Y2_GPIO_NUM    32
+#define VSYNC_GPIO_NUM 22
+#define HREF_GPIO_NUM  26
+#define PCLK_GPIO_NUM  21
+
+#elif defined(CAMERA_MODEL_M5STACK_V2_PSRAM)
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM 15
+#define XCLK_GPIO_NUM  27
+#define SIOD_GPIO_NUM  22
+#define SIOC_GPIO_NUM  23
+
+#define Y9_GPIO_NUM    19
+#define Y8_GPIO_NUM    36
+#define Y7_GPIO_NUM    18
+#define Y6_GPIO_NUM    39
+#define Y5_GPIO_NUM    5
+#define Y4_GPIO_NUM    34
+#define Y3_GPIO_NUM    35
+#define Y2_GPIO_NUM    32
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM  26
+#define PCLK_GPIO_NUM  21
+
+#elif defined(CAMERA_MODEL_M5STACK_WIDE)
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM 15
+#define XCLK_GPIO_NUM  27
+#define SIOD_GPIO_NUM  22
+#define SIOC_GPIO_NUM  23
+
+#define Y9_GPIO_NUM    19
+#define Y8_GPIO_NUM    36
+#define Y7_GPIO_NUM    18
+#define Y6_GPIO_NUM    39
+#define Y5_GPIO_NUM    5
+#define Y4_GPIO_NUM    34
+#define Y3_GPIO_NUM    35
+#define Y2_GPIO_NUM    32
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM  26
+#define PCLK_GPIO_NUM  21
+
+#define LED_GPIO_NUM 2
+
+#elif defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM 15
+#define XCLK_GPIO_NUM  27
+#define SIOD_GPIO_NUM  25
+#define SIOC_GPIO_NUM  23
+
+#define Y9_GPIO_NUM    19
+#define Y8_GPIO_NUM    36
+#define Y7_GPIO_NUM    18
+#define Y6_GPIO_NUM    39
+#define Y5_GPIO_NUM    5
+#define Y4_GPIO_NUM    34
+#define Y3_GPIO_NUM    35
+#define Y2_GPIO_NUM    17
+#define VSYNC_GPIO_NUM 22
+#define HREF_GPIO_NUM  26
+#define PCLK_GPIO_NUM  21
+
+#elif defined(CAMERA_MODEL_M5STACK_UNITCAM)
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM 15
+#define XCLK_GPIO_NUM  27
+#define SIOD_GPIO_NUM  25
+#define SIOC_GPIO_NUM  23
+
+#define Y9_GPIO_NUM    19
+#define Y8_GPIO_NUM    36
+#define Y7_GPIO_NUM    18
+#define Y6_GPIO_NUM    39
+#define Y5_GPIO_NUM    5
+#define Y4_GPIO_NUM    34
+#define Y3_GPIO_NUM    35
+#define Y2_GPIO_NUM    32
+#define VSYNC_GPIO_NUM 22
+#define HREF_GPIO_NUM  26
+#define PCLK_GPIO_NUM  21
+
+#elif defined(CAMERA_MODEL_AI_THINKER)
+#define PWDN_GPIO_NUM  32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  0
+#define SIOD_GPIO_NUM  26
+#define SIOC_GPIO_NUM  27
+
+#define Y9_GPIO_NUM    35
+#define Y8_GPIO_NUM    34
+#define Y7_GPIO_NUM    39
+#define Y6_GPIO_NUM    36
+#define Y5_GPIO_NUM    21
+#define Y4_GPIO_NUM    19
+#define Y3_GPIO_NUM    18
+#define Y2_GPIO_NUM    5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM  23
+#define PCLK_GPIO_NUM  22
+
+// 4 for flash led or 33 for normal led
+#define LED_GPIO_NUM   4
+
+#elif defined(CAMERA_MODEL_TTGO_T_JOURNAL)
+#define PWDN_GPIO_NUM  0
+#define RESET_GPIO_NUM 15
+#define XCLK_GPIO_NUM  27
+#define SIOD_GPIO_NUM  25
+#define SIOC_GPIO_NUM  23
+
+#define Y9_GPIO_NUM    19
+#define Y8_GPIO_NUM    36
+#define Y7_GPIO_NUM    18
+#define Y6_GPIO_NUM    39
+#define Y5_GPIO_NUM    5
+#define Y4_GPIO_NUM    34
+#define Y3_GPIO_NUM    35
+#define Y2_GPIO_NUM    17
+#define VSYNC_GPIO_NUM 22
+#define HREF_GPIO_NUM  26
+#define PCLK_GPIO_NUM  21
+
+#elif defined(CAMERA_MODEL_XIAO_ESP32S3)
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  10
+#define SIOD_GPIO_NUM  40
+#define SIOC_GPIO_NUM  39
+
+#define Y9_GPIO_NUM    48
+#define Y8_GPIO_NUM    11
+#define Y7_GPIO_NUM    12
+#define Y6_GPIO_NUM    14
+#define Y5_GPIO_NUM    16
+#define Y4_GPIO_NUM    18
+#define Y3_GPIO_NUM    17
+#define Y2_GPIO_NUM    15
+#define VSYNC_GPIO_NUM 38
+#define HREF_GPIO_NUM  47
+#define PCLK_GPIO_NUM  13
+
+#elif defined(CAMERA_MODEL_ESP32_CAM_BOARD)
+// The 18 pin header on the board has Y5 and Y3 swapped
+#define USE_BOARD_HEADER 0
+#define PWDN_GPIO_NUM    32
+#define RESET_GPIO_NUM   33
+#define XCLK_GPIO_NUM    4
+#define SIOD_GPIO_NUM    18
+#define SIOC_GPIO_NUM    23
+
+#define Y9_GPIO_NUM 36
+#define Y8_GPIO_NUM 19
+#define Y7_GPIO_NUM 21
+#define Y6_GPIO_NUM 39
+#if USE_BOARD_HEADER
+#define Y5_GPIO_NUM 13
+#else
+#define Y5_GPIO_NUM 35
+#endif
+#define Y4_GPIO_NUM 14
+#if USE_BOARD_HEADER
+#define Y3_GPIO_NUM 35
+#else
+#define Y3_GPIO_NUM 13
+#endif
+#define Y2_GPIO_NUM    34
+#define VSYNC_GPIO_NUM 5
+#define HREF_GPIO_NUM  27
+#define PCLK_GPIO_NUM  25
+
+#elif defined(CAMERA_MODEL_ESP32S3_CAM_LCD)
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  40
+#define SIOD_GPIO_NUM  17
+#define SIOC_GPIO_NUM  18
+
+#define Y9_GPIO_NUM    39
+#define Y8_GPIO_NUM    41
+#define Y7_GPIO_NUM    42
+#define Y6_GPIO_NUM    12
+#define Y5_GPIO_NUM    3
+#define Y4_GPIO_NUM    14
+#define Y3_GPIO_NUM    47
+#define Y2_GPIO_NUM    13
+#define VSYNC_GPIO_NUM 21
+#define HREF_GPIO_NUM  38
+#define PCLK_GPIO_NUM  11
+
+#elif defined(CAMERA_MODEL_ESP32S2_CAM_BOARD)
+// The 18 pin header on the board has Y5 and Y3 swapped
+#define USE_BOARD_HEADER 0
+#define PWDN_GPIO_NUM    1
+#define RESET_GPIO_NUM   2
+#define XCLK_GPIO_NUM    42
+#define SIOD_GPIO_NUM    41
+#define SIOC_GPIO_NUM    18
+
+#define Y9_GPIO_NUM 16
+#define Y8_GPIO_NUM 39
+#define Y7_GPIO_NUM 40
+#define Y6_GPIO_NUM 15
+#if USE_BOARD_HEADER
+#define Y5_GPIO_NUM 12
+#else
+#define Y5_GPIO_NUM 13
+#endif
+#define Y4_GPIO_NUM 5
+#if USE_BOARD_HEADER
+#define Y3_GPIO_NUM 13
+#else
+#define Y3_GPIO_NUM 12
+#endif
+#define Y2_GPIO_NUM    14
+#define VSYNC_GPIO_NUM 38
+#define HREF_GPIO_NUM  4
+#define PCLK_GPIO_NUM  3
+
+#elif defined(CAMERA_MODEL_ESP32S3_EYE)
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  15
+#define SIOD_GPIO_NUM  4
+#define SIOC_GPIO_NUM  5
+
+#define Y2_GPIO_NUM 11
+#define Y3_GPIO_NUM 9
+#define Y4_GPIO_NUM 8
+#define Y5_GPIO_NUM 10
+#define Y6_GPIO_NUM 12
+#define Y7_GPIO_NUM 18
+#define Y8_GPIO_NUM 17
+#define Y9_GPIO_NUM 16
+
+#define VSYNC_GPIO_NUM 6
+#define HREF_GPIO_NUM  7
+#define PCLK_GPIO_NUM  13
+
+#elif defined(CAMERA_MODEL_DFRobot_FireBeetle2_ESP32S3) || defined(CAMERA_MODEL_DFRobot_Romeo_ESP32S3)
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  45
+#define SIOD_GPIO_NUM  1
+#define SIOC_GPIO_NUM  2
+
+#define Y9_GPIO_NUM    48
+#define Y8_GPIO_NUM    46
+#define Y7_GPIO_NUM    8
+#define Y6_GPIO_NUM    7
+#define Y5_GPIO_NUM    4
+#define Y4_GPIO_NUM    41
+#define Y3_GPIO_NUM    40
+#define Y2_GPIO_NUM    39
+#define VSYNC_GPIO_NUM 6
+#define HREF_GPIO_NUM  42
+#define PCLK_GPIO_NUM  5
+*/
+#else
+#error "Camera model not selected"
+#endif
+
+/* Constant defines -------------------------------------------------------- */
+#define EI_CAMERA_RAW_FRAME_BUFFER_COLS           320
+#define EI_CAMERA_RAW_FRAME_BUFFER_ROWS           240
+#define EI_CAMERA_FRAME_BYTE_SIZE                 3
+
+/* Private variables ------------------------------------------------------- */
+static bool debug_nn = false; 
+static bool is_initialised = false;
+uint8_t *snapshot_buf; 
+
+static camera_config_t camera_config = {
+    .pin_pwdn = PWDN_GPIO_NUM,
+    .pin_reset = RESET_GPIO_NUM,
+    .pin_xclk = XCLK_GPIO_NUM,
+    .pin_sscb_sda = SIOD_GPIO_NUM,
+    .pin_sscb_scl = SIOC_GPIO_NUM,
+
+    .pin_d7 = Y9_GPIO_NUM,
+    .pin_d6 = Y8_GPIO_NUM,
+    .pin_d5 = Y7_GPIO_NUM,
+    .pin_d4 = Y6_GPIO_NUM,
+    .pin_d3 = Y5_GPIO_NUM,
+    .pin_d2 = Y4_GPIO_NUM,
+    .pin_d1 = Y3_GPIO_NUM,
+    .pin_d0 = Y2_GPIO_NUM,
+    .pin_vsync = VSYNC_GPIO_NUM,
+    .pin_href = HREF_GPIO_NUM,
+    .pin_pclk = PCLK_GPIO_NUM,
+
+    //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_QVGA,    //QQVGA-UXGA Do not use sizes above QVGA when not JPEG
+
+    .jpeg_quality = 12, //0-63 lower number means higher quality
+    .fb_count = 1,       //if more than one, i2s runs in continuous mode. Use only with JPEG
+    .fb_location = CAMERA_FB_IN_PSRAM,
+    .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+};
+
+/* Function definitions ------------------------------------------------------- */
+bool ei_camera_init(void);
+void ei_camera_deinit(void);
+bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf) ;
+
+/**
+* @brief      Arduino setup function
+*/
+void setup()
+{
+    Serial.begin(115200);
+
+    // Uncomment to use SD Card
+    /*SD_MMC.setPins(SD_MMC_CLK, SD_MMC_CMD, SD_MMC_D0);
+    if(!SD_MMC.begin("/sdcard", true, true, SDMMC_FREQ_DEFAULT, 5)){
+      Serial.println("Card Mount Failed");
+      return;
+    }
+    uint8_t cardType = SD_MMC.cardType();
+    if(cardType == CARD_NONE){
+      Serial.println("No SD_MMC card attached");
+      return;
+    }*/
+
+    //createDir(SD_MMC, "/photos");
+    //comment out the below line to start inference immediately after upload
+    while (!Serial);
+    Serial.println("Edge Impulse Inferencing Demo");
+    if (ei_camera_init() == false) {
+        ei_printf("Failed to initialize Camera!\r\n");
+    }
+    else {
+        ei_printf("Camera initialized\r\n");
+    }
+
+    // Define diodes as output
+    pinMode(PIN_BLUE, OUTPUT);
+    pinMode(PIN_GREEN, OUTPUT);
+    pinMode(PIN_RED, OUTPUT);
+
+    // Turn off all the diodes
+    digitalWrite(PIN_BLUE, LOW);
+    digitalWrite(PIN_GREEN, LOW);
+    digitalWrite(PIN_RED, LOW);
+
+    ei_printf("\nStarting continious inference in 2 seconds...\n");
+    ei_sleep(2000);
+}
+
+/**
+* @brief      Get data and run inferencing
+*
+* @param[in]  debug  Get debug info if true
+*/
+void loop()
+{
+    if (ei_sleep(5) != EI_IMPULSE_OK) {
+        return;
+    }
+
+    snapshot_buf = (uint8_t*)malloc(EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS * EI_CAMERA_FRAME_BYTE_SIZE);
+
+    // check if allocation was successful
+    if(snapshot_buf == nullptr) {
+        ei_printf("ERR: Failed to allocate snapshot buffer!\n");
+        return;
+    }
+
+    ei::signal_t signal;
+    signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
+    signal.get_data = &ei_camera_get_data;
+
+    if (ei_camera_capture((size_t)EI_CLASSIFIER_INPUT_WIDTH, (size_t)EI_CLASSIFIER_INPUT_HEIGHT, snapshot_buf) == false) {
+        ei_printf("Failed to capture image\r\n");
+        free(snapshot_buf);
+        return;
+    }
+
+    // Run the classifier
+    ei_impulse_result_t result = { 0 };
+
+    EI_IMPULSE_ERROR err = run_classifier(&signal, &result, debug_nn);
+    if (err != EI_IMPULSE_OK) {
+        ei_printf("ERR: Failed to run classifier (%d)\n", err);
+        return;
+    }
+
+    // print the predictions
+    ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
+                result.timing.dsp, result.timing.classification, result.timing.anomaly);
+
+#if EI_CLASSIFIER_OBJECT_DETECTION == 1
+    bool bb_found = result.bounding_boxes[0].value > 0;
+    for (size_t ix = 0; ix < result.bounding_boxes_count; ix++) {
+        auto bb = result.bounding_boxes[ix];
+        if (bb.value == 0) {
+            continue;
+        }
+        ei_printf("    %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\n", bb.label, bb.value, bb.x, bb.y, bb.width, bb.height);
+    }
+    if (!bb_found) {
+        ei_printf("    No objects found\n");
+    }
+#else
+    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+        ei_printf("    %s: %.5f\n", result.classification[ix].label,
+                                    result.classification[ix].value);
+    }
+
+    // Turn off all the diodes
+    digitalWrite(PIN_RED, LOW);
+    digitalWrite(PIN_BLUE, LOW);
+    digitalWrite(PIN_GREEN, LOW);
+
+    // Take the values of classes probability
+    float class1_prob = result.classification[0].value; // Manuel
+    float class2_prob = result.classification[1].value; // Samuele
+    float class3_prob = result.classification[2].value; // Sconosciuto
+
+    if(class1_prob > 0.50){
+      digitalWrite(PIN_RED, LOW);
+      digitalWrite(PIN_GREEN, LOW);
+      digitalWrite(PIN_BLUE, HIGH);
+    }
+    else{
+      if(class2_prob > 0.50){
+        digitalWrite(PIN_GREEN, HIGH);
+        digitalWrite(PIN_RED, LOW);
+        digitalWrite(PIN_BLUE, LOW);
+      }
+      else{
+        digitalWrite(PIN_RED, HIGH);
+        digitalWrite(PIN_GREEN, LOW);
+        digitalWrite(PIN_BLUE, LOW);
+      }
+    }
+#endif
+
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+        ei_printf("    anomaly score: %.3f\n", result.anomaly);
+#endif
+
+
+    free(snapshot_buf);
+
+}
+
+/**
+ * @brief   Setup image sensor & start streaming
+ *
+ * @retval  false if initialisation failed
+ */
+bool ei_camera_init(void) {
+
+    if (is_initialised) return true;
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+    // Initialize the camera
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK) {
+      Serial.printf("Camera init failed with error 0x%x\n", err);
+      return false;
+    }
+
+    sensor_t * s = esp_camera_sensor_get();
+    // Initial sensors are flipped vertically and colors are a bit saturated
+    if (s->id.PID == OV3660_PID) {
+      s->set_vflip(s, 1); // flip it back
+      s->set_brightness(s, 1); // up the brightness 
+      s->set_saturation(s, 0); // lower the saturation
+    }
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE)
+    s->set_vflip(s, 1);
+    s->set_hmirror(s, 1);
+#elif defined(CAMERA_MODEL_ESP_EYE)
+    s->set_vflip(s, 1);
+    s->set_hmirror(s, 1);
+    s->set_awb_gain(s, 1);
+#endif
+
+    is_initialised = true;
+    return true;
+}
+
+/**
+ * @brief      Stop streaming of sensor data
+ */
+void ei_camera_deinit(void) {
+
+    //deinitialize the camera
+    esp_err_t err = esp_camera_deinit();
+
+    if (err != ESP_OK)
+    {
+        ei_printf("Camera deinit failed\n");
+        return;
+    }
+
+    is_initialised = false;
+    return;
+}
+
+
+/**
+ * @brief      Capture, rescale and crop image
+ *
+ * @param[in]  img_width     width of output image
+ * @param[in]  img_height    height of output image
+ * @param[in]  out_buf       pointer to store output image, NULL may be used
+ *                           if ei_camera_frame_buffer is to be used for capture and resize/cropping.
+ *
+ * @retval     false if not initialised, image captured, rescaled or cropped failed
+ *
+ */
+bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf) {
+    bool do_resize = false;
+
+    if (!is_initialised) {
+        ei_printf("ERR: Camera is not initialized\r\n");
+        return false;
+    }
+
+    camera_fb_t *fb = esp_camera_fb_get();
+
+    if (!fb) {
+        ei_printf("Camera capture failed\n");
+        return false;
+    }
+
+    // Uncomment to save the last image captured on SD Card
+    /*String path = "/photos/photo.jpg";
+
+    fs::FS &fs = SD_MMC; 
+  
+    File file = fs.open(path.c_str(), FILE_WRITE);
+    if(!file){
+      Serial.println("Failed to open file in writing mode");
+    } 
+    else {
+      file.write(fb->buf, fb->len); 
+      Serial.printf("Saved file to path: %s\n", path.c_str());
+    }
+    file.close();*/
+
+   bool converted = fmt2rgb888(fb->buf, fb->len, PIXFORMAT_JPEG, snapshot_buf);
+
+   esp_camera_fb_return(fb);
+
+   if(!converted){
+       ei_printf("Conversion failed\n");
+       return false;
+   }
+
+    if ((img_width != EI_CAMERA_RAW_FRAME_BUFFER_COLS)
+        || (img_height != EI_CAMERA_RAW_FRAME_BUFFER_ROWS)) {
+        do_resize = true;
+    }
+
+    if (do_resize) {
+        ei::image::processing::crop_and_interpolate_rgb888(
+        out_buf,
+        EI_CAMERA_RAW_FRAME_BUFFER_COLS,
+        EI_CAMERA_RAW_FRAME_BUFFER_ROWS,
+        out_buf,
+        img_width,
+        img_height);
+    }
+
+
+    return true;
+}
+
+static int ei_camera_get_data(size_t offset, size_t length, float *out_ptr)
+{
+    // we already have a RGB888 buffer, so recalculate offset into pixel index
+    size_t pixel_ix = offset * 3;
+    size_t pixels_left = length;
+    size_t out_ptr_ix = 0;
+
+    while (pixels_left != 0) {
+        // Swap BGR to RGB here
+        // due to https://github.com/espressif/esp32-camera/issues/379
+        out_ptr[out_ptr_ix] = (snapshot_buf[pixel_ix + 2] << 16) + (snapshot_buf[pixel_ix + 1] << 8) + snapshot_buf[pixel_ix];
+
+        // go to the next pixel
+        out_ptr_ix++;
+        pixel_ix+=3;
+        pixels_left--;
+    }
+    // and done!
+    return 0;
+}
+
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_CAMERA
+#error "Invalid model for current sensor"
+#endif
